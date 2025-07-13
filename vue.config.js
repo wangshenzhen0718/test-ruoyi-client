2bb1899a1fa42fe64c9172a7eaca74d489268847
wsz <94780184@qq.com>
2025-07-13 21:42:52 +0800

    feat: 👈 关闭 ESLint 保存时检查

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false, // 👈 关闭 ESLint 保存时检查
 })
