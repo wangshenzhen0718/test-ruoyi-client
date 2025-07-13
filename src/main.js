b024b75138029b2536506c6a45fde4a1ce296d5d
wsz <94780184@qq.com>
2025-07-13 22:01:28 +0800

    feat: 引入路由组件、element-ui组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引入vue-router
+import VueRouter from "vue-router";
+import router from './router';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 
+
+//应用vue-router插件
+Vue.use(VueRouter);
+//应用element-ui插件
+Vue.use(ElementUI);
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+//该文件专门用于创建整个应用的路由器
+
+import VueRouter from "vue-router";
+
+
+//创建并默认暴露一个路由器
+export default new VueRouter({
+    routes:[
+
+    ]
+});
