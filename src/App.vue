fd0c84fe1c84deb7ad7b0cacd3c87a6035050a7f
wsz <94780184@qq.com>
2025-07-14 10:05:04 +0800

    feat: 拆分App页为Home页，防止后续修改App

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,15 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-xs-offset-2 col-xs-8">
-        <div class="page-header"><h2>路由切换练习</h2></div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-2 col-xs-offset-2">
-        <el-menu
-            class="el-menu-demo"
-            mode="horizontal"
-            :default-active="$route.path"
-            router
-        >
-          <el-menu-item index="/page1">典型表单</el-menu-item>
-          <el-menu-item index="/page2">基础表格</el-menu-item>
-        </el-menu>
-      </div>
-      <div class="col-xs-6">
-        <div class="panel">
-          <div class="panel-body">
-            <!-- router-view 确定视图的位置 -->
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Home></Home>
   </div>
 </template>
 
 <script>
-import Page1 from "@/components/Page1.vue";
-import Page2 from "@/components/Page2.vue";
+import Home from "@/components/Home.vue";
 
 export default {
   name: "App",
-  components: { Page1, Page2 },
+  components: {Home},
 }
 </script>
 
---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>主页</h2></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <el-menu
+            class="el-menu-demo"
+            mode="horizontal"
+            :default-active="$route.path"
+            router
+        >
+          <el-menu-item index="/page1">典型表单</el-menu-item>
+          <el-menu-item index="/page2">基础表格</el-menu-item>
+        </el-menu>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!-- router-view 确定视图的位置 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Page1 from "@/components/Page1.vue";
+import Page2 from "@/components/Page2.vue";
+
+export default {
+  name: "Home",
+  components: {Page1, Page2},
+}
+</script>
+
